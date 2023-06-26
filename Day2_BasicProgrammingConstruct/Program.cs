875f033b3669d6ed41743d2a099f47347f3d9035
Subhash Barela <barelasubhash16@gmail.com>
2023-06-26 15:41:11 +0530

    [subhash] Day2 Practice Problem

---
+++ Day2_BasicProgrammingConstruct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_BasicProgrammingConstruct
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
